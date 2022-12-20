266417b96757c7f0ab41878a5b47233a50771a08
unknown <Charlotte Landuyt>
2022-12-20 09:44:41 +0100

    exo

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+let nombre = 8.56;
+console.log(Math.round(nombre));
+
+console.log(Math.ceil(nombre));
+
+console.log(Math.floor(nombre));
+
+console.log(Math.trunc(nombre));
+
+console.log(Math.random());
+
+console.log(Math.random()*101);
+
+console.log(Math.round(Math.random()*101));
+
+console.log(Math.pow(8,2));
+
+console.log(Math.sqrt(9));
+
+console.log(Math.abs(-1));
+
+let suite = [-2, 1000, 8, 57];
+
+console.log(Math.max(...suite));
+console.log(Math.min(...suite));
+
